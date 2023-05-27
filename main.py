df74c9e932b27005dc6c0fa059970228d3f1ddcb
David Lopez <3936234+davidnoelopez@users.noreply.github.com>
2023-05-26 20:16:02 -0600

    fastf1 test

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 import os
+from flask import Flask
+from matplotlib import pyplot as plt
+import fastf1
+import fastf1.plotting
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import io
 
 app = Flask(__name__)
 
@@ -7,6 +14,30 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+fastf1.plotting.setup_mpl()
+
+@app.route("/api/lap_plot")
+def lap_plot():
+    session = fastf1.get_session(2019, 'Monza', 'Q')
+
+    session.load()
+    fast_leclerc = session.laps.pick_driver('LEC').pick_fastest()
+    lec_car_data = fast_leclerc.get_car_data()
+    t = lec_car_data['Time']
+    vCar = lec_car_data['Speed']
+
+    # Plot the data
+
+    fig = Figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Speed (km/h)')
+    ax.set_title('Leclerc fastest lap at Monza 2019')
+    ax.plot(t, vCar)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 if __name__ == '__main__':
